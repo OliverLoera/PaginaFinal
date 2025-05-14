4d5ada8999d2d92217388b7a7a9f342281ad353f
OliverLoera <oliverleonelloera@gmail.com>
2025-05-14 07:49:52 -0600

    Update index.php

--- index.php
+++ index.php
@@ -99,7 +99,7 @@
 
     <footer>
         <p>&copy; 2025 Felcor Transportes. Todos los derechos reservados.</p>
-        <p><a href="#">Política de privacidad</a> | <a href="#">Términos y condiciones</a></p>
+        <p><a href="mapa.php">Mapa de Sitio</a> | <a href="creditos.php">Creditos</a></p>
     </footer>
 
     <script>
